9baec30d1a7302a09624a2bccdd1314173dfe9d7
Diney Oliveira <diney.oliveira>
2023-01-12 11:20:33 -0300

    criando screens

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './src/screens/Home';
+import Detail from './src/screens/Detail';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen  name='Home' component={Home}/>
+        <Stack.Screen name='Detail' component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
